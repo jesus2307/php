b8bddd02b3ed1e56222f3b9d62690683a147abd3
jesus2307 <jesusfernandezrueda09@gmail.com>
2021-02-22 14:53:49 +0100

    ejer

---
+++ Ejercicios de introducción/ejer2.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Direcciond ip :" . $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo "Software :" . $_SERVER['SERVER_SOFTWARE'];
+echo "<br>";
+echo "User agent :" . $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo "Ip cliente :" . $_SERVER['REMOTE_ADDR'];
+
+?>
---
+++ Ejercicios de introducción/ejer3.php
@@ -0,0 +1,43 @@
+<?php
+
+echo "<h1>\$_SERVER</h1>";
+echo "<pre>";
+print_r($_SERVER);
+echo "<pre>";
+
+echo "<h1>\$_GET</h1>";
+echo "<pre>";
+print_r($_GET);
+echo "<pre>";
+
+echo "<h1>\$_POST</h1>";
+echo "<pre>";
+print_r($_POST);
+echo "<pre>";
+
+echo "<h1>\$_FILES</h1>";
+echo "<pre>";
+print_r($_FILES);
+echo "<pre>";
+
+echo "<h1>\$_COOKIE</h1>";
+echo "<pre>";
+print_r($_COOKIE);
+echo "<pre>";
+
+echo "<h1>\$_SESSION</h1>";
+echo "<pre>";
+print_r($_SESSION);
+echo "<pre>";
+
+echo "<h1>\$_REQUEST</h1>";
+echo "<pre>";
+print_r($_REQUEST);
+echo "<pre>";
+
+echo "<h1>\$_ENV</h1>";
+echo "<pre>";
+print_r($_ENV);
+echo "<pre>";
+
+?>
