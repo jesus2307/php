bda5922ea70238f6e5033fe1f8316c200bb5c561
jesus <60348759+jesus2307@users.noreply.github.com>
2021-02-23 14:38:11 +0100

    Update ejercicio7.php

--- Ejercicios de introducción/ejercicios/ejercicio7.php
+++ Ejercicios de introducción/ejercicios/ejercicio7.php
@@ -2,6 +2,6 @@
 /*7. Escribe un script PHP que realice la simulación de lanzar dos dados y muestre una imagen con los valores obtenidos en cada uno de los dados.*/
 $dado1 = rand(1,6);
 $dado2 = rand(1,6);
-echo "<img src=\"https://upload.wikimedia.org/wikipedia/commons/1/1b/Dice-1-b.svg">";
-echo "<img src=\"https://upload.wikimedia.org/wikipedia/commons/5/5f/Dice-2-b.svg">";
+echo "<img src=\"imagenes/$dado1.svg\">";
+echo "<img src=\"imagenes/$dado2.svg\">";
 ?>
