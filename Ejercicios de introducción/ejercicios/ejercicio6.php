6753752eb783f2c83f71ff3ee7e4236d38f5a2bb
jesus <60348759+jesus2307@users.noreply.github.com>
2021-02-23 14:38:49 +0100

    Update ejercicio6.php

--- Ejercicios de introducción/ejercicios/ejercicio6.php
+++ Ejercicios de introducción/ejercicios/ejercicio6.php
@@ -1,4 +1,5 @@
 <?php
 /*6. Escribe un script PHP que realice la simulación de lanzar un dado y muestre una imagen con un valor aleatorio enre 1 y resuelva el ejercicio sin utilizar las estructuras de control if - else y switch.*/
-
+$numero = rand(1,6);
+echo "<img src=\"imagenes/$numero.svg\">";
 ?>
