66521674ce969e168ab59d469d33167e102fa2ac
jesus <60348759+jesus2307@users.noreply.github.com>
2021-02-23 14:54:53 +0100

    Update ejercicio5.php

--- Ejercicios de introducción/ejercicios/ejercicio5.php
+++ Ejercicios de introducción/ejercicios/ejercicio5.php
@@ -1,4 +1,24 @@
 <?php
 /*5. Escribe un script PHP que realice la simulación de lanzar un dado y muestre una imagen con un valor aleatorio entre 1 y resuelva el ejercicio utilizando la estructura de control switch.*/
-
+switch ($numero) {
+    case 1:
+        echo "<img src=\"imagenes/1.svg\">";
+        break;
+    case 2:
+        echo "<img src=\"imagenes/2.svg\">";
+         break;
+    case 3:
+        echo "<img src=\"imagenes/3.svg\">";
+        break;
+    case 4:
+        echo "<img src=\"imagenes/4.svg\">";
+        break;
+    case 5:
+        echo "<img src=\"imagenes/5.svg\">";
+        break;
+    case 6:
+        echo "<img src=\"imagenes/6.svg\">";
+        break;
+}
+?>
 ?>
