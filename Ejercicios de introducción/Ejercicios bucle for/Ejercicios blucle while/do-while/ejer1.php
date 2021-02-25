f6e23f532251f164ad350dc8991d1edb3e5e243d
jesus <60348759+jesus2307@users.noreply.github.com>
2021-02-25 13:31:51 +0100

    Update ejer1.php

--- Ejercicios de introducción/Ejercicios bucle for/Ejercicios blucle while/do-while/ejer1.php
+++ Ejercicios de introducción/Ejercicios bucle for/Ejercicios blucle while/do-while/ejer1.php
@@ -1,9 +1,24 @@
-<?php
-/*1. Escribe un script PHP que muestre los números del 1 al 10 en una tabla de una fila y 10 columnas. Utiliza un bucle while*/
-$a = 1;
-do {
-    echo "<td> $a </td>";
-    $a++
-}
-    while ($a<=10);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejer 1</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <?php
+             /*1. Escribe un script PHP que muestre los números del 1 al 10 en una tabla de una fila y 10 columnas. Utiliza un bucle while*/
+            $a = 1;
+            do {
+                echo "<td> $a </td>";
+                $a++
+            }
+                while ($a<=10);               
+            ?>
+        </tr>
+    </table>
+</body>
+</html>
