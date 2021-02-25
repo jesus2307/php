3df52fb33717bab8e4e5fc1806d078c3c9ecb67e
jesus <60348759+jesus2307@users.noreply.github.com>
2021-02-25 12:16:55 +0100

    Update ejer4.php

--- Ejercicios de introducción/Ejercicios bucle for/ejer4.php
+++ Ejercicios de introducción/Ejercicios bucle for/ejer4.php
@@ -1,4 +1,15 @@
 <?php
 /*4. Escribe un script PHP que muestre la tabla de multiplicar de un número aleatorio. Utiliza un bucle for*/
-
+$numero=rand(1, 10);
+echo "<table>";
+for ($i=0; $i <=10; $i++){    
+    echo "<tr>";
+        echo "<td> $numero </td>";
+        echo "<td> x </td>";
+        echo "<td> $i </td>";
+        echo "<td> = </td>";
+        echo "<td>".$numero*$i."</td>"; // para unir variables a la cadena de texto se usan los .variable.
+    echo "</tr>";
+}
+echo "</table>";
 ?>
