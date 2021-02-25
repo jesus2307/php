65e154272e5a83da362aec87189e3d6cece9acf7
jesus <60348759+jesus2307@users.noreply.github.com>
2021-02-25 13:16:55 +0100

    Update ejer3.php

--- Ejercicios de introducción/Ejercicios bucle for/ejer3.php
+++ Ejercicios de introducción/Ejercicios bucle for/ejer3.php
@@ -1,10 +1,23 @@
-<?php
-/*3. Escribe un script PHP que muestre en una tabla los números pares que existen entre 1 y 100. Utiliza un bucle for.*/
-echo "<table border='1'>";
-for ($i = 2; $i <= 100; $i= $i+2) {
-echo "<tr>;";
-echo "<td> $i </td>";
-echo "</tr>;";
-}
-echo "</table>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejer 2</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <?php
+          /*3. Escribe un script PHP que muestre en una tabla los números pares que existen entre 1 y 100. Utiliza un bucle for.*/
+           for ($i = 2; $i <= 100; $i= $i+2) {
+            echo "<tr>;";
+            echo "<td> $i </td>";
+            echo "</tr>;";
+            }
+            ?>
+        </tr>
+    </table>
+</body>
+</html>
