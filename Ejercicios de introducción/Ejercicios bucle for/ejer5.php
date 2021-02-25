514fba6aa877040e470559fd98faf6bcd654ef34
jesus <60348759+jesus2307@users.noreply.github.com>
2021-02-25 13:21:38 +0100

    Update ejer5.php

--- Ejercicios de introducción/Ejercicios bucle for/ejer5.php
+++ Ejercicios de introducción/Ejercicios bucle for/ejer5.php
@@ -1,17 +1,30 @@
-<?php
-/*5. Escribe un script PHP que muestre las tablas de multiplicar del 1 al 10. Utiliza un bucle for*/
-$numero=rand(1, 10);
-echo "<table>";
-for ($numero = 1; $numero<=10; $numero++;){
-    for ($i=0; $i <=10; $i++){    
-        echo "<tr>";
-            echo "<td> $numero </td>";
-            echo "<td> x </td>";
-            echo "<td> $i </td>";
-            echo "<td> = </td>";
-            echo "<td>".$numero*$i."</td>"; // para unir variables a la cadena de texto se usan los .variable.
-        echo "</tr>";
-    }
-}
-echo "</table>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejer 5</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <?php
+     /*5. Escribe un script PHP que muestre las tablas de multiplicar del 1 al 10. Utiliza un bucle for*/
+     $numero=rand(1, 10);
+         for ($numero = 1; $numero<=10; $numero++;){
+            for ($i=0; $i <=10; $i++){    
+                echo "<tr>";
+                echo "<td> $numero </td>";
+                echo "<td> x </td>";
+                echo "<td> $i </td>";
+                echo "<td> = </td>";
+                echo "<td>".$numero*$i."</td>"; /*unir variables */
+                echo "</tr>";
+            }
+        }
+            ?>
+        </tr>
+    </table>
+</body>
+</html>
