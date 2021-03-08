8ae77dbba28183cc0d63f6d14d08ac37b23abf22
jesus <60348759+jesus2307@users.noreply.github.com>
2021-03-08 13:19:03 +0100

    Update ejer8.php

--- Ejercicios de introducción/arrays/ejer8.php
+++ Ejercicios de introducción/arrays/ejer8.php
@@ -1,13 +1,3 @@
 <?php
-/* DECLARAR ARRAY*/
-$lista = array();
-/* INICIAR ARRAY*/
-for ($i = 0; $i < 10; $i++) {
-    $lista[$i] = rand(1,30);
-}
-/* MOSTRAR ARRAY */
-for ($i = 0; $i < 10; $i++) {
-    echo $lista[$i];
-    echo "<br>";
-}
+
 ?>
