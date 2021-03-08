538a118941c7858cf4b3bb80d12b9f5de1c84d17
jesus <60348759+jesus2307@users.noreply.github.com>
2021-03-08 13:29:34 +0100

    Update ejer9.php

--- Ejercicios de introducción/arrays/ejer9.php
+++ Ejercicios de introducción/arrays/ejer9.php
@@ -1,10 +1,39 @@
 <?php
 /*9. Escribe un script PHP que permita ordenar el siguiente array asociativo:
-
 array("Antonio"=>"31", "María"=>"28", "Juan"=>"29", "Pepe"=>"27")
-De forma ascendente ordenado por valor.
-De forma ascendente ordenado por clave.
-De forma descendente ordenado por valor.
-De forma descendetne ordenado por clave.*/
+*/
 
+list = array("Antonio"=>"31", "María"=>"28", "Juan"=>"29", "Pepe"=>"27")
+echo '<h2>De forma ascendente ordenado por valor.</h2>'
+asort($lista);
+
+foreach ($lista as $clave => $valor){
+echo 'clave' $clave = valor $valor 
+echo "<br>"
+}
+
+list = array("Antonio"=>"31", "María"=>"28", "Juan"=>"29", "Pepe"=>"27")
+echo '<h2>De forma ascendente ordenado por clave.</h2>'
+arsort($lista);
+
+foreach ($lista as $clave => $valor){
+echo 'clave' $clave = valor $valor 
+echo "<br>"
+}
+list = array("Antonio"=>"31", "María"=>"28", "Juan"=>"29", "Pepe"=>"27")
+echo '<h2>De forma descendente ordenado por valor.</h2>'
+ksort($lista);
+
+foreach ($lista as $clave => $valor){
+echo 'De forma descendetne ordenado por clave.' $clave = valor $valor 
+echo "<br>"
+}
+list = array("Antonio"=>"31", "María"=>"28", "Juan"=>"29", "Pepe"=>"27")
+echo '<h2>Valor</h2>'
+krsort($lista);
+
+foreach ($lista as $clave => $valor){
+echo 'clave' $clave = valor $valor 
+echo "<br>"
+}
 ?>
